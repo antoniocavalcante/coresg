054505dd0ef76d020b2124bf7b717e1134c2a540
Antonio Cavalcante <ant.cavalcante@gmail.com>
2021-01-20 19:03:41 -0700

    Prim implementation is working

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
+import numpy as np
 
+from sklearn.neighbors import NearestNeighbors
+
+from mst import mst
+
+from scipy.spatial.distance import pdist, squareform
+from scipy.sparse.csgraph import minimum_spanning_tree
 
 if __name__ == "__main__":
-    pass
+    
+    # generates a 
+    k = 2
+
+    # generates a small random dataset
+    data = np.random.rand(10,2)
+    
+    # computes core-distances and knn information
+    nbrs = NearestNeighbors(n_neighbors=k, algorithm='ball_tree').fit(data)    
+    core_distances, knn = nbrs.kneighbors(data)
+
+    # computes MST based on the local implementation of Prim
+    mst_prim = mst.prim(data, core_distances, k, False)
+    print(mst_prim)
+    print(mst_prim.sum())
+    X = squareform(pdist(data, 'euclidean'))
+
+    print(" ---------------------- ")
+
+    # computes MST based on the scikit-learn package
+    mst_scip = minimum_spanning_tree(X)
+    print(mst_scip)
+    print(mst_prim.sum())
+
+    # compares the total weight of both graphs
+    if mst_prim.sum() == mst_scip.sum():
+        print("Pass!")
+    else:
+        print("Something is wrong!")
--- mst/mst.py
+++ mst/mst.py
@@ -7,6 +7,8 @@ def prim(data, core_distances, min_pts, self_edges):
 
     n = len(data)
 
+    n_edges = 2*n - 1 if self_edges else n - 1
+
     # keeps track of which points are attached to the tree.
     attached = np.zeros(n)
 
@@ -17,13 +19,13 @@ def prim(data, core_distances, min_pts, self_edges):
     current_point = n - 1
 
     # arrays to keep track of the shortest connection to each point.
-    nearest_points = np.zeros(n-1)
-    nearest_distances  = np.full(n-1, np.inf)
+    nearest_points = np.zeros(n_edges)
+    nearest_distances  = np.full(n_edges, np.inf)
 
     while (num_edges < n - 1):
 
         # keeps track of the closest point to the tree.
-        nearest_distance = -1
+        nearest_distance = np.inf
         nearest_point = -1
 
         # marks current point as attached
@@ -31,13 +33,13 @@ def prim(data, core_distances, min_pts, self_edges):
 
         # loops over the dataset to find the next point to attach.
         for neighbor in range(n):    
-            if attached(neighbor): continue
+            if attached[neighbor]: continue
 
-            distance = distance.euclidean(data[current_point], data[neighbor])
+            d = distance.euclidean(data[current_point], data[neighbor])
 
             # updates the closese point to neigbor.
-            if distance < nearest_distances[neighbor]:
-                nearest_distances[neighbor] = distance
+            if d < nearest_distances[neighbor]:
+                nearest_distances[neighbor] = d
                 nearest_points[neighbor] = current_point
             
             # updates the closest point to the tree. 
@@ -52,7 +54,8 @@ def prim(data, core_distances, min_pts, self_edges):
         # updates the number of edges added.
         num_edges += 1
 
-    if self_edges:
-        nearest_points[n:] = np.arange(n)
-
+    # if self_edges:
+        # nearest_points[n-1:] = np.arange(n)
+        # nearest_distances[n-1:] = [ distance.euclidean(data[i], data[i]) for i in range(n)]
+    
     return coo_matrix((nearest_distances, (nearest_points, np.arange(n-1))), shape=(n, n))
