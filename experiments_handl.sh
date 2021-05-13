77a7a2384caff360ab6db4a40abf75e882c22579
Antonio Cavalcante <ant.cavalcante@gmail.com>
2021-05-13 14:52:35 -0300

    fixing scripts

--- experiments_handl.sh
+++ experiments_handl.sh
@@ -7,8 +7,8 @@ DEFAULT_DIM=32
 DEFAULT_DATA=10
 DEFAULT_CLUS=30
 
-KNN=false
-KNN_INC=false
+CORE=false
+CORE_INC=false
 RNG=false
 ALL=false
 
@@ -16,16 +16,16 @@ dataset() {
 
     for n in 1 5 10 50 100;
     do
-        if $KNN || $ALL ; then
-            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${n}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "knn" >> "knn-hdbscan-dataset.results"
+        if $CORE || $ALL ; then
+            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${n}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "knn" >> "handl-core-dataset.results"
         fi
 
-        if $KNN_INC || $ALL ; then
-            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${n}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "knn_inc" >> "knn-inc-hdbscan-dataset.results"
+        if $CORE_INC || $ALL ; then
+            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${n}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "knn_inc" >> "handl-core-inc-dataset.results"
         fi
         
         if $RNG || $ALL ; then
-            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${n}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "rng" >> "rng-hdbscan-dataset.results"
+            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${n}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "rng" >> "handl-rng-dataset.results"
         fi
     done
 }
@@ -34,16 +34,16 @@ minpoints() {
 
     for minpoints in 20 40 60 80 100;
     do
-        if $KNN || $ALL ; then
-            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" ${minpoints} " " "knn" >> "knn-hdbscan-minpoints.results"
+        if $CORE || $ALL ; then
+            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" ${minpoints} " " "knn" >> "handl-core-minpoints.results"
         fi
 
-        if $KNN_INC || $ALL ; then
-            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" ${minpoints} " " "knn_inc" >> "knn-inc-hdbscan-minpoints.results"
+        if $CORE_INC || $ALL ; then
+            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" ${minpoints} " " "knn_inc" >> "handl-core-inc-minpoints.results"
         fi
 
         if $RNG || $ALL ; then
-            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" ${minpoints} " " "rng" >> "rng-hdbscan-minpoints.results"
+            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" ${minpoints} " " "rng" >> "handl-rng-minpoints.results"
         fi
     done
 }
@@ -52,16 +52,16 @@ dimensions() {
 
     for dim in 4 8 16 32 64 128;
     do
-        if $KNN || $ALL ; then
-            python main_experiments.py "${DIR}/${dim}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "knn" >> "knn-hdbscan-dimensions.results"
+        if $CORE || $ALL ; then
+            python main_experiments.py "${DIR}/${dim}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "knn" >> "handl-core-dimensions.results"
         fi
 
-        if $KNN_INC || $ALL ; then
-            python main_experiments.py "${DIR}/${dim}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "knn_inc" >> "knn-inc-hdbscan-dimensions.results"
+        if $CORE_INC || $ALL ; then
+            python main_experiments.py "${DIR}/${dim}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "knn_inc" >> "handl-core-inc-dimensions.results"
         fi
 
         if $RNG || $ALL ; then
-            python main_experiments.py "${DIR}/${dim}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "rng" >> "rng-hdbscan-dimensions.results"
+            python main_experiments.py "${DIR}/${dim}d-${DEFAULT_DATA}n-${DEFAULT_CLUS}c.dat" "${DEFAULT_KMAX}" " " "rng" >> "handl-rng-dimensions.results"
         fi
     done
 }
@@ -71,17 +71,40 @@ clusters() {
 
     for clus in 10 20 30;
     do
-        if $KNN || $ALL ; then
-            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${clus}c.dat" "${DEFAULT_KMAX}" " " "knn" >> "knn-hdbscan-clusters.results"
+        if $CORE || $ALL ; then
+            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${clus}c.dat" "${DEFAULT_KMAX}" " " "knn" >> "handl-core-clusters.results"
         fi
 
-        if $KNN_INC || $ALL ; then
-            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${clus}c.dat" "${DEFAULT_KMAX}" " " "knn_inc" >> "knn-inc-hdbscan-clusters.results"
+        if $CORE_INC || $ALL ; then
+            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${clus}c.dat" "${DEFAULT_KMAX}" " " "knn_inc" >> "handl-core-inc-clusters.results"
         fi
 
         if $RNG || $ALL ; then
-            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${clus}c.dat" "${DEFAULT_KMAX}" " " "rng" >> "rng-hdbscan-clusters.results"
+            python main_experiments.py "${DIR}/${DEFAULT_DIM}d-${DEFAULT_DATA}n-${clus}c.dat" "${DEFAULT_KMAX}" " " "rng" >> "handl-rng-clusters.results"
         fi
+    done
+}
+
+initial() {
+
+    for n in 5 10 50;
+    do
+        for dim in 4 8 16 32 64 128;
+        do
+            for kmax in 10 30 50;
+            do
+                for c in 10 30 50;
+                do
+                    if $CORE || $ALL ; then
+                        python main_experiments.py "${DIR}/${dim}d-${n}n-${c}c.dat" "${kmax}" " " "knn" >> "handl-core-initial-${n}n-${kmax}k-${c}c.results"
+                    fi
+                    
+                    if $RNG || $ALL ; then
+                        python main_experiments.py "${DIR}/${dim}d-${n}n-${c}c.dat" "${kmax}" " " "rng" >> "handl-rng-star-initial-${n}n-${kmax}k-${c}c.results"
+                    fi
+                done
+            done
+        done
     done
 }
 
@@ -92,17 +115,17 @@ then
     exit 1
 fi
 
-if [[ ${2^^} == "KNN" || $2 == "ALL" ]]; then
+if [[ ${2^^} == "CORE" || $2 == "ALL" ]]; then
     KNN=true
 fi
-if [[ ${2^^} == "KNN_INC" || $2 == "ALL" ]]; then
+if [[ ${2^^} == "CORE_INC" || $2 == "ALL" ]]; then
     KNN_INC=true
 fi
 if [[ ${2^^} == "RNG" || $2 == "ALL" ]]; then
     RNG=true
 fi
 
-ALL=[[ $KNN && $KNN_INC && $RNG ]]
+ALL=[[ $CORE && $CORE_INC && $RNG ]]
 
 SECONDS=0
 for i in $(seq 1)
@@ -111,6 +134,7 @@ do
 	minpoints
 	dataset
     clusters
+    initial
 done
 
 DURATION=$SECONDS
