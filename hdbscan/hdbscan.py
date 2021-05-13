Temporary fix to precision issue.

hdbscan/hdbscan.py
@@ -52,10 +52,17 @@ class HDBSCAN:
             self.knn = np.load(name + "-" + str(min_pts) + "-knn.npy")
         except:
             from sklearn.neighbors import NearestNeighbors
+            from mst.mst import euclidean_export
+
             nbrs = NearestNeighbors(n_neighbors=min_pts).fit(self.data)
 
-            # computes the core-distances and knn information
+            # computes the core-distances and knn information.
             self.core_distances, self.knn = nbrs.kneighbors(self.data)
+
+            # fix core-distances to use the same precision as the local function.
+            for i in range(self.n):
+                for j in range(1, self.min_pts):
+                    self.core_distances[i, j] = euclidean_export(self.data[i], self.data[self.knn[i, j]])
 
             # saving the computed core-distances, knn and knng on files.
             np.save(name + "-" + str(self.min_pts) + ".cd" , self.core_distances)